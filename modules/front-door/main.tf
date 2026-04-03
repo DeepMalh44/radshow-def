e3c3226fd2865276fae5f23f463978e9b07ab0a2
Ketaanh Shah <ketaanhshah@microsoft.com>
2026-04-02 21:42:18 -0400

    fix(front-door): correct restore_traffic_time attribute name (endpoints → endpoint)

--- modules/front-door/main.tf
+++ modules/front-door/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_cdn_frontdoor_origin_group" "this" {
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.this.id
   session_affinity_enabled = each.value.session_affinity_enabled
 
-  restore_traffic_time_to_healed_or_new_endpoints_in_minutes = each.value.restore_traffic_time_to_healed_or_new_endpoints_in_minutes
+  restore_traffic_time_to_healed_or_new_endpoint_in_minutes = each.value.restore_traffic_time_to_healed_or_new_endpoint_in_minutes
 
   health_probe {
     interval_in_seconds = each.value.health_probe.interval_in_seconds
